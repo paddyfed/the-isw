fda6e356b9ff03c52834a6bdb837744631c9dc76
Paddy <paddyfedoo@hotmail.com>
2023-10-11 00:01:31 +0100

    Deploying correctly

--- app/page.js
+++ app/page.js
@@ -3,22 +3,22 @@ import { getData } from "./utils/apiHelpers";
 import { LongDateFormat } from "./components/dateComponents";
 
 export default async function Home() {
-  const api = `https://www.anapioficeandfire.com/api/characters`;
+  const api = `${process.env.API_URL}api/`;
 
   const data = await getData(api);
 
   return (
     <>
       <h1>Updates</h1>
-      {data.map((x) => {
+      {data.newsposts.map((x) => {
         return (
           <article key={x.id}>
             <h1>
               <time dateTime={x.date}>{LongDateFormat(x.date)}</time>
             </h1>
-            {/* {x.entry.map((y, index) => {
+            {x.entry.map((y, index) => {
               return <p key={index}>{y}</p>;
-            })} */}
+            })}
           </article>
         );
       })}
