c57fb1f722fa857cb2840bdfcf4b6d30474ae429
Paddy <paddyfedoo@hotmail.com>
2023-10-10 22:47:12 +0100

    Show last 4 posts on main screen

--- app/api/route.js
+++ app/api/route.js
@@ -2,5 +2,8 @@ import { NextResponse } from "next/server";
 import newsposts from "@/json/newsposts.json";
 
 export async function GET() {
-  return NextResponse.json({ newsposts: newsposts });
+  const filtered = newsposts
+    .sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+    .slice(0, 4);
+  return NextResponse.json({ newsposts: filtered });
 }
--- app/components/headBanner.js
+++ app/components/headBanner.js
@@ -3,8 +3,20 @@ import Image from "next/image";
 export default function HeadBanner() {
   return (
     <header>
-      <Image src="/yoda.png" width={229} height={200} alt="Yoda" />
-      <Image src="/main_banner.png" width={530} height={200} alt="ISW" />
+      <Image
+        src="/yoda.png"
+        width={229}
+        height={200}
+        alt="Yoda"
+        priority={true}
+      />
+      <Image
+        src="/main_banner.png"
+        width={530}
+        height={200}
+        alt="ISW"
+        priority={true}
+      />
     </header>
   );
 }
