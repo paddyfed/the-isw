65857f63edf99b0c07686b8c3b62b02abc412042
Paddy <paddyfedoo@hotmail.com>
2023-10-23 23:19:50 +0100

    Remove console log

--- app/archive_news/[YearMonth]/page.js
+++ app/archive_news/[YearMonth]/page.js
@@ -5,7 +5,6 @@ export default async function ArchiveNewsYearMonth({ params }) {
   const api = `${process.env.API_URL}/api/archive_news/${params.YearMonth}`;
   const allPostsData = await getData(api);
 
-  console.log("allpostsdata", allPostsData);
   if (allPostsData.filtered.length === 0) return `No Data for Today`;
 
   return (
