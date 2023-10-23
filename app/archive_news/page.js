4b0e78a36f381046a38f0b3596cb4a459efb3dfe
Paddy <paddyfedoo@hotmail.com>
2023-10-23 21:25:29 +0100

    News Archive Index - Grid

--- app/archive_news/page.js
+++ app/archive_news/page.js
@@ -1,19 +1,51 @@
 import { getPostsArchiveIndex } from "../utils/postsHelpers";
 import Link from "next/link";
-const hellow = await getPostsArchiveIndex();
+import pageStyles from "./page.module.css";
+
+const inputArray = await getPostsArchiveIndex();
+
+const hellow = inputArray.reduce((acc, curr) => {
+  const yearObject = acc.find((item) => item.year === curr.year);
+  if (yearObject) {
+    yearObject.months.push(curr.month);
+  } else {
+    acc.push({ months: [curr.month], year: curr.year });
+  }
+  return acc;
+}, []);
+
+function getMonthName(monthNumber) {
+  const date = new Date();
+  date.setMonth(monthNumber - 1);
+
+  return date.toLocaleString("en-US", {
+    month: "long",
+  });
+}
+
 export default async function NewsArchive() {
   return (
     <>
-      {hellow.map(({ month, year }) => {
-        const newMonth = month + 1 > 10 ? month + 1 : `0${month + 1}`;
-        return (
-          <p key={`${year}${newMonth}`}>
-            <Link href={`/archive_news/${year}-${newMonth}`}>
-              {year} {newMonth}
-            </Link>
-          </p>
-        );
-      })}
+      <h1>News Archives</h1>
+      {hellow.map((item) => (
+        <article key={item.year} className={pageStyles.archiveGrid}>
+          <h2 className={pageStyles.archiveYear}>{item.year}</h2>
+          {item.months.map((month) => {
+            const newMonth = month + 1 > 10 ? month + 1 : `0${month + 1}`;
+            const monthText = getMonthName(month + 1);
+            return (
+              <span
+                key={newMonth}
+                className={`pageStyles.archiveMonth${newMonth}`}
+              >
+                <Link href={`/archive_news/${item.year}-${newMonth}`}>
+                  {monthText}
+                </Link>
+              </span>
+            );
+          })}
+        </article>
+      ))}
     </>
   );
 }
